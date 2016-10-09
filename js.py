755384d3febae07a30706b300d27fbf047521127
lleii <lilei@live.com>
2016-10-09 20:19:45 +0800

    init

--- js.py
+++ js.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 @app.route("/")
 def show_tables():
 
+	
+
+	
 	if os.path.isfile('db.xlsx'):
 		data = pd.read_excel('db.xlsx','doing')
 	elif os.path.isfile('/Volumes/data/pt/db.xlsx'):
@@ -18,10 +21,27 @@ def show_tables():
 	else:
 		print("error")
 		pass
+	#hcol=['db_images','db_title','db_rating','mi_duration','db_countries','db_genres', 'db_subtype','db_year',  'db_summary',]
+	hcol=['db_rating','mi_duration','db_countries','db_genres', 'db_subtype','db_year', ]
+	data=data[hcol]
+	l='['
+	#m=list()
+	for i,r in data.iterrows():
+		if i>3 :
+			continue
+
+		l=l+'['
+		for j in data.columns :
+			l=l+'"'+str(r[j])+'",'
+		l=l+'],\n'
+	l=l+']'
+	
+
+	print (l)
 
 
 	
-	hcol=['db_images','db_title','db_rating','mi_duration','db_countries','db_genres', 'db_subtype','db_year',  'db_summary',]
+	
 #        o[o.status != 'done'][col].sort_values(['db_rating'],ascending=0).to_excel(writer, sheet_name='doing')
 #        o[o.status == 'done'][col].sort_values(['db_rating'],ascending=0).to_excel(writer, sheet_name='done')
 
@@ -31,12 +51,17 @@ def show_tables():
     #males = data.loc[data.Gender=='m']
 
 
-	print(data[hcol].to_json('db.json'))
+	#print(data[hcol].to_json('db.json'))
 
 
-	pd.set_option('display.max_colwidth', -1)
+	#pd.set_option('display.max_colwidth', -1)
 
-	return render_template('movman.html', table=data[hcol].to_html(escape=False))
+	return render_template('js.html', table=l)
 
 if __name__ == "__main__":
+
+
+
+
+
     app.run(debug=True,host='0.0.0.0')
